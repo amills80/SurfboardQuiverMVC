6a277506a8985c23f24ce60fa9c370559ca23693
Alan Mills <aelonrench@yahoo.com>
2018-02-12 13:58:47 -0500

    Add project files.

---
+++ SurfboardQuiver/Controllers/SurfboardController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SurfboardQuiver.Controllers
+{
+    public class SurfboardController : Controller
+    {
+
+        public ActionResult Details()
+        {
+            ViewBag.Make = "McTavish";
+            ViewBag.Model = "Sumo";
+            ViewBag.Length = 7.8;
+            ViewBag.Style = "Midlength";
+            ViewBag.Fins = "Single";
+            ViewBag.Notes = "Sporty, fun 2+1 board. Great for pitchy and clean-face waves.";
+            ViewBag.Image = @"C:\Users\aelon\SurfboardQuiver\SurfboardQuiver\images\McTavish_Sumo.jpg";
+            ViewBag.DisplayText = ViewBag.Make + ": " + ViewBag.Model + " - " + ViewBag.Length + "'";
+
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SurfboardQuiver/Models/Surfboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurfboardQuiver.Models
+{
+    public class Surfboard
+    {
+        public int Id { get; set; }
+        public Builder Make { get; set; }
+        public string Model { get; set; }
+        public string Shape { get; set; }
+        public string FinSetup { get; set; }
+        public float Length { get; set; }
+        public float Width { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+    }
+}
