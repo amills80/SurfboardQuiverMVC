da46e9fe88143bfba82504285374d274e82e2a25
Alan Mills <aelonrench@yahoo.com>
2018-02-21 14:05:00 -0500

    Change fin setup and board shape properties in model from string to enum. Change fin setup and board shape fields from text box to drop down menu.

--- SurfboardQuiver/Models/Surfboard.cs
+++ SurfboardQuiver/Models/Surfboard.cs
@@ -5,15 +5,35 @@ using System.Web;
 
 namespace SurfboardQuiver.Models
 {
+    public enum BoardShape
+    {
+        Longboard, 
+        Midlength, 
+        Fish,
+        Hybrid,
+        Shortboard,
+        Other
+    }
+
+    public enum FinSetup
+    {
+        Single,
+        Two_Plus_One,
+        Twin,
+        Keel,
+        Quad,
+        Thruster,
+        Bonzer,
+        Other
+    }
+
     public class Surfboard
     {
         public int Id { get; set; }
         public Builder Make { get; set; }
         public string Model { get; set; }
-        // TODO: Change shape from string to enum for preset values. 
-        // Then change createBoard page to use a drop down instead of a text box.
-        public string Shape { get; set; }
-        public string FinSetup { get; set; }
+        public BoardShape Shape { get; set; }
+        public FinSetup FinSetup { get; set; }
         public float Length { get; set; }
         public float Width { get; set; }
         public string Description { get; set; }
